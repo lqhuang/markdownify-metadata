08dc3e96445c154cb53ed35d165903c72a09a0e2
Lanqing Huang <lqhuang@outlook.com>
2023-08-27 15:57:12 +0800

    chore: Add tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,41 @@
-import { defineConfig } from "vite";
-import webExtension from "@samrum/vite-plugin-web-extension";
+import type { Manifest } from 'webextension-polyfill-ts'
 
-const manifest: chrome.runtime.Manifest = {
+import { defineConfig } from 'vite'
+import webExtension from '@samrum/vite-plugin-web-extension'
+
+const manifest: Manifest.WebExtensionManifest = {
   manifest_version: 3,
-  name: "markify-webinfo",
-  description: "Extract website info into markdown link",
-  version: "0.0.1",
+  name: 'markify-webinfo',
+  description: 'Extract website info into markdown link',
+  version: '0.0.1',
   icons: {
-    "64": "icons/icon.png",
+    '64': 'icons/icon.png',
   },
   content_scripts: [
     {
-      matches: ["*://github.com/*"],
-      js: ["src/content_script.js"],
+      matches: ['*://github.com/*'],
+      js: ['src/content_script.js'],
     },
   ],
   action: {
     default_icon: {
-      "64": "icons/icon.png",
+      '64': 'icons/icon.png',
     },
-    default_popup: "src/action/index.html",
-    default_title: "markify",
+    default_popup: 'src/action/index.html',
+    default_title: 'markify',
   },
   options_ui: {
-    page: "src/options/index.html",
+    page: 'src/options/index.html',
   },
-};
+}
+
+const isDev = process.env.NODE_ENV !== 'production'
+const isFirefox = process.env.EXTENSION === 'firefox'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    "import.meta.vitest": "undefined",
+    'import.meta.vitest': 'undefined',
   },
   resolve: {
     alias: {
@@ -42,8 +47,8 @@ export default defineConfig({
       manifest,
     }),
   ],
-  assetsInclude: ["icons/**.png"],
+  assetsInclude: ['icons/**.png'],
   build: {
     // assetsDir: "assets",
   },
-});
+})

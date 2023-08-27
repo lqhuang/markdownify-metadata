710543803d1172e367fb3bb15b89d68c80457b34
Lanqing Huang <lqhuang@outlook.com>
2023-08-27 15:46:13 +0800

    chore: Add vite as bundle tool

---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from "vite";
+import webExtension from "@samrum/vite-plugin-web-extension";
+
+const manifest: chrome.runtime.Manifest = {
+  manifest_version: 3,
+  name: "markify-webinfo",
+  description: "Extract website info into markdown link",
+  version: "0.0.1",
+  icons: {
+    "64": "icons/icon.png",
+  },
+  content_scripts: [
+    {
+      matches: ["*://github.com/*"],
+      js: ["src/content_script.js"],
+    },
+  ],
+  action: {
+    default_icon: {
+      "64": "icons/icon.png",
+    },
+    default_popup: "src/action/index.html",
+    default_title: "markify",
+  },
+  options_ui: {
+    page: "src/options/index.html",
+  },
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    "import.meta.vitest": "undefined",
+  },
+  resolve: {
+    alias: {
+      src: `${__dirname}/src`,
+    },
+  },
+  plugins: [
+    webExtension({
+      manifest,
+    }),
+  ],
+  assetsInclude: ["icons/**.png"],
+  build: {
+    // assetsDir: "assets",
+  },
+});

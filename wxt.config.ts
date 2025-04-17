d55f6e9548365ced3508d6a12924fec9a5188235
Lanqing Huang <lqhuang@outlook.com>
2025-04-17 16:20:51 +0800

    chore: Switch ext framework to `wxt`

---
+++ wxt.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'wxt'
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  srcDir: 'src',
+  modules: ['@wxt-dev/module-react'],
+  // react: {},
+  manifest: {
+    name: 'markdownify-metadata',
+    action: {},
+    permissions: [
+      'storage',
+      'unlimitedStorage',
+      'sidePanel',
+      'declarativeNetRequestWithHostAccess',
+      'scripting',
+      'tabs',
+      'activeTab',
+      'clipboardWrite',
+    ],
+    host_permissions: ['https://github.com'],
+    optional_host_permissions: ['https://*/*', 'wss://*/*'],
+  },
+})
